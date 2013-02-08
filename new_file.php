145e9809a1570697fcb5a0d35396b4d850dc6a9b
tomdwallace <tom.wallace@cancer.org.uk>
2013-02-08 16:34:12 +0000

    2md commit new branch

---
+++ new_file.php
@@ -0,0 +1,26 @@
+<?php
+
+// Original PHP code by Chirp Internet: www.chirp.com.au // Please acknowledge use of this code by including this header.
+
+ function textTruncate($string, $limit, $break = ".", $pad = "...") {
+    // return with no change if string is shorter than $limit
+
+    if (strlen($string) <= $limit) return $string;
+
+    // is $break present between $limit and the end of the string?
+
+    if (false !== ($breakpoint = strpos($string, $break, $limit))) {
+
+        if ($breakpoint < strlen($string) - 1) {
+
+            $string = substr($string, 0, $breakpoint) . $pad;
+        }
+    } return $string;
+
+}
+
+// Test File
+
+echo textTruncate('Some text here not truncated', 100);
+echo 'My first remote '
+?>
